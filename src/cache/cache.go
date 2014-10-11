55ff9bba50599ffb8d5134956cd86e9897429254
Luis Pabon <lpabon@redhat.com>
2014-10-10 21:22:08 -0400

    Cleaned up cachemap.
    It now includes tests
    
    Signed-off-by: Luis Pabon <lpabon@redhat.com>

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2014 The Cache Authors
+// Copyright (c) 2014 The pblcache Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- src/cache/cachemap_test.go
+++ src/cache/cachemap_test.go
@@ -15,40 +15,37 @@
 //
 package cache
 
-/*
 import (
 	"testing"
 )
 
-func TestAlloc(t *testing.T) {
+func TestInsert(t *testing.T) {
 	cmap := NewCacheMap(2)
 
-	id := AddressMapKey{1, 2}
-	cmap.SetAddressMapKey(id, 3)
-	assert(t, cmap.HasAddressMapKey(id))
-
-	index := cmap.Alloc(id)
-	assert(t, cmap.bds[0].address == id)
+	id := uint64(123)
+	index, evictkey, evict := cmap.Insert(id)
+	assert(t, cmap.bds[0].key == id)
 	assert(t, cmap.bds[0].mru == false)
 	assert(t, cmap.bds[0].used == true)
 	assert(t, index == 0)
+	assert(t, evictkey == INVALID_KEY)
+	assert(t, evict == false)
 }
 
 func TestUsing(t *testing.T) {
 	cmap := NewCacheMap(2)
 
-	id := AddressMapKey{1, 2}
-	cmap.SetAddressMapKey(id, 3)
-	assert(t, cmap.HasAddressMapKey(id))
-
-	index := cmap.Alloc(id)
-	assert(t, cmap.bds[0].address == id)
+	id := uint64(123)
+	index, evictkey, evict := cmap.Insert(id)
+	assert(t, cmap.bds[0].key == id)
 	assert(t, cmap.bds[0].mru == false)
 	assert(t, cmap.bds[0].used == true)
 	assert(t, index == 0)
+	assert(t, evictkey == INVALID_KEY)
+	assert(t, evict == false)
 
 	cmap.Using(index)
-	assert(t, cmap.bds[0].address == id)
+	assert(t, cmap.bds[0].key == id)
 	assert(t, cmap.bds[0].mru == true)
 	assert(t, cmap.bds[0].used == true)
 }
@@ -56,15 +53,14 @@ func TestUsing(t *testing.T) {
 func TestFree(t *testing.T) {
 	cmap := NewCacheMap(2)
 
-	id := AddressMapKey{1, 2}
-	cmap.SetAddressMapKey(id, 3)
-	assert(t, cmap.HasAddressMapKey(id))
-
-	index := cmap.Alloc(id)
-	assert(t, cmap.bds[0].address == id)
+	id := uint64(123)
+	index, evictkey, evict := cmap.Insert(id)
+	assert(t, cmap.bds[0].key == id)
 	assert(t, cmap.bds[0].mru == false)
 	assert(t, cmap.bds[0].used == true)
 	assert(t, index == 0)
+	assert(t, evictkey == INVALID_KEY)
+	assert(t, evict == false)
 
 	cmap.Free(index)
 	assert(t, cmap.bds[0].mru == false)
@@ -74,66 +70,57 @@ func TestFree(t *testing.T) {
 func TestEvictions(t *testing.T) {
 	cmap := NewCacheMap(2)
 
-	id1 := AddressMapKey{1, 2}
-	id2 := AddressMapKey{1, 3}
-	id3 := AddressMapKey{2, 2}
+	id1 := uint64(123)
+	id2 := uint64(456)
+	id3 := uint64(678)
 
-	index := cmap.Alloc(id1)
-	cmap.SetAddressMapKey(id1, index)
-	assert(t, cmap.HasAddressMapKey(id1))
-	assert(t, !cmap.HasAddressMapKey(id2))
-	assert(t, !cmap.HasAddressMapKey(id3))
-	assert(t, cmap.bds[0].address == id1)
+	index, evictkey, evict := cmap.Insert(id1)
+	assert(t, cmap.bds[0].key == id1)
 	assert(t, cmap.bds[0].mru == false)
 	assert(t, cmap.bds[0].used == true)
 	assert(t, index == 0)
+	assert(t, evictkey == INVALID_KEY)
+	assert(t, evict == false)
 
-	index = cmap.Alloc(id2)
-	cmap.SetAddressMapKey(id2, index)
-	assert(t, cmap.HasAddressMapKey(id1))
-	assert(t, cmap.HasAddressMapKey(id2))
-	assert(t, !cmap.HasAddressMapKey(id3))
-	assert(t, cmap.bds[1].address == id2)
+	index, evictkey, evict = cmap.Insert(id2)
+	assert(t, cmap.bds[0].key == id1)
+	assert(t, cmap.bds[0].mru == false)
+	assert(t, cmap.bds[0].used == true)
+	assert(t, cmap.bds[1].key == id2)
 	assert(t, cmap.bds[1].mru == false)
 	assert(t, cmap.bds[1].used == true)
 	assert(t, index == 1)
+	assert(t, evictkey == INVALID_KEY)
+	assert(t, evict == false)
 
 	cmap.Using(0)
-	assert(t, cmap.bds[0].address == id1)
+	assert(t, cmap.bds[0].key == id1)
 	assert(t, cmap.bds[0].mru == true)
 	assert(t, cmap.bds[0].used == true)
 
-	index = cmap.Alloc(id3)
-	cmap.SetAddressMapKey(id3, index)
-	assert(t, cmap.HasAddressMapKey(id1))
-	assert(t, !cmap.HasAddressMapKey(id2))
-	assert(t, cmap.HasAddressMapKey(id3))
-	assert(t, cmap.bds[0].address == id1)
+	index, evictkey, evict = cmap.Insert(id3)
+	assert(t, cmap.bds[0].key == id1)
 	assert(t, cmap.bds[0].mru == false)
 	assert(t, cmap.bds[0].used == true)
-	assert(t, cmap.bds[1].address == id3)
+	assert(t, cmap.bds[1].key == id3)
 	assert(t, cmap.bds[1].mru == false)
 	assert(t, cmap.bds[1].used == true)
 	assert(t, index == 1)
+	assert(t, evictkey == id2)
+	assert(t, evict == true)
 
 	cmap.Free(1)
-	assert(t, cmap.HasAddressMapKey(id1))
-	assert(t, !cmap.HasAddressMapKey(id2))
-	assert(t, !cmap.HasAddressMapKey(id3))
 	assert(t, cmap.bds[1].mru == false)
 	assert(t, cmap.bds[1].used == false)
 
-	index = cmap.Alloc(id2)
-	cmap.SetAddressMapKey(id2, index)
-	assert(t, !cmap.HasAddressMapKey(id1))
-	assert(t, cmap.HasAddressMapKey(id2))
-	assert(t, !cmap.HasAddressMapKey(id3))
-	assert(t, cmap.bds[0].address == id2)
+	index, evictkey, evict = cmap.Insert(id2)
+	assert(t, cmap.bds[0].key == id2)
 	assert(t, cmap.bds[0].mru == false)
 	assert(t, cmap.bds[0].used == true)
 	assert(t, cmap.bds[1].mru == false)
 	assert(t, cmap.bds[1].used == false)
 	assert(t, index == 0)
+	assert(t, evictkey == id1)
+	assert(t, evict == true)
 
 }
-*/
