1cf72e3e66a731051f17455ee6defb0cd9449536
Luis Pabón <lpabon@redhat.com>
2015-01-28 13:06:17 -0500

    pblio: Graphing tool uses RRDtool
    Signed-off-by: Luis Pabón <lpabon@redhat.com>

--- apps/pblio/pblioplot.py
+++ apps/pblio/pblioplot.py
@@ -20,10 +20,7 @@ import json
 import rrdtool
 
 # Default input is every 5 secs
-In_Period=5
-
-# Default output here is set to 30min
-Out_Period=30*60
+Period=5
 
 # Convert from pblio.data -> pblio.csv
 fp = open('pblio.data', 'r')
@@ -32,30 +29,79 @@ jsondata = json.loads(line)
 fp.close()
 
 # Setup info
-data_sources = ['DS:tlat_duration:COUNTER:600:0:U',
-				'DS:tlat_counter:COUNTER:600:0:U']
+data_sources = ['DS:tlat_d:COUNTER:600:0:U',
+				'DS:tlat_c:COUNTER:600:0:U',
+				'DS:cache_hits:COUNTER:600:0:U',
+				'DS:cache_reads:COUNTER:600:0:U',
+				]
 
 # Create db
 rrdtool.create('pblio.rrd',
-	'--start', "%d" % (jsondata['time']),
-	'--step', '5',
+	'--start', "%d" % (jsondata['time']-Period),
+	'--step', '%d' % (Period),
 	data_sources,
-	'RRA:AVERAGE:0.5:6:300',
-	'RRA:AVERAGE:0.5:360:1200',
-	'RRA:AVERAGE:0.5:1440:1200')
+	'RRA:AVERAGE:0.5:120:2600',
+	'RRA:AVERAGE:0.5:360:2600')
+
+# Open JSON data file
+fp = open('pblio.data', 'r')
 
 # Cover data
-rrdtime=In_Period+jsondata['time']
-rrd = None
-
-fp = open('pblio.data', 'r')
+start_time=jsondata['time']
+rrdtime = start_time
+end_time=0
 for line in fp.readlines():
 	stat = json.loads(line)
+
+	# Calculations
+	tlat_d = int(stat['spc']['total']['latency']['duration']/1000)
+	tlat_c = stat['spc']['total']['latency']['count']
+
+	# Get cache
+	try:
+		cache_hits = stat['cache']['readhits']
+		cache_reads = stat['cache']['reads']
+	except:
+		cache_hits = 0
+		cache_reads = 0
+
+	# Enter into rrd
 	rrdtool.update('pblio.rrd',
 		("%d:" % rrdtime) +
-		("%d:" % stat['spc']['total']['latency']['duration']) +
-		("%d" % stat['spc']['total']['latency']['count']))
-	rrdtime+=In_Period
+		("%d:" % tlat_d)+
+		("%d:" % tlat_c)+
+		("%d:" % cache_hits)+
+		("%d" % cache_reads))
+	rrdtime += Period
+
+	# Save the end time for graphs
+	end_time = stat['time']
 
 fp.close()
 
+# Graph Total Latency
+rrdtool.graph('tlat.png',
+	'--start', '%d' % start_time,
+	'--end', '%d' % end_time,
+	'-w 800',
+	'-h 400',
+	'--vertical-label=Time (ms)',
+	'DEF:duration=pblio.rrd:tlat_d:AVERAGE',
+	'DEF:count=pblio.rrd:tlat_c:AVERAGE',
+	'CDEF:latms=duration,count,/,1000,/',
+	'LINE2:latms#FF0000:Total Latency')
+
+# Graph Read Hit Rate
+rrdtool.graph('readhit.png',
+	'--start', '%d' % start_time,
+	'--end', '%d' % end_time,
+	'-w 800',
+	'-h 400',
+	'--vertical-label=Percentage',
+	'DEF:hits=pblio.rrd:cache_hits:AVERAGE',
+	'DEF:reads=pblio.rrd:cache_reads:AVERAGE',
+	'CDEF:readhit=hits,reads,/,100,*',
+	'LINE2:readhit#FF0000:Cache Read Hit')
+
+print "Start Time = %d" % (start_time)
+print "End Time = %d" % (end_time)
