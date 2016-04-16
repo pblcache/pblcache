bdab80127004c6488c723b7ca0ef5241cf886b25
Luis Pabón <lpabon@redhat.com>
2016-04-16 19:26:16 -0400

    Fixed unit test failure

--- cache/log.go
+++ cache/log.go
@@ -41,7 +41,7 @@ const (
 	MB                   = 1024 * KB
 	GB                   = 1024 * MB
 	TB                   = 1024 * GB
-	NumberSegmentBuffers = 512
+	NumberSegmentBuffers = 32
 )
 
 // Allows these functions to be mocked by tests
